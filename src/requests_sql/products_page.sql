0fe6c23b45ccf9452fe22586797e6869fd05077c
Sergey Borodin <bsy.prog@gmail.com>
2022-10-24 20:24:05 +0300

    Fix request

--- src/requests_sql/products_page.sql
+++ src/requests_sql/products_page.sql
@@ -2,7 +2,7 @@
 -- (name of products, number of units on sale, supplier contact name and phone number)
 SELECT product_name, units_in_stock, suppliers.contact, suppliers.phone
 FROM products
-JOIN categories ON products.category_id = products.category_id
+JOIN categories ON categories.category_id = products.category_id
 JOIN suppliers ON suppliers.id = products.fk_suppliers
 WHERE categories.category_name in ('Beverages', 'Seafood')
 	  AND products.discontinued = 0
